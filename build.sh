2197a46df18202e1d55bf188d16057b613de8a14
UmarBot <2t78zmwktx@privaterelay.appleid.com>
2025-12-05 06:09:57 +0600

    Delete build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# УСТАНАВЛИВАЕМ PYTHON 3.10 РУКАМИ
-pyenv install -s 3.10.13
-pyenv global 3.10.13
-
-# Устанавливаем зависимости
-pip install --upgrade pip
-pip install -r requirements.txt
